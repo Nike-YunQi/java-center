7d5efda0f6d1ef3257bb4764575119d65683c159
mos <mos@mosdeMacBook-Pro.local>
2020-03-26 00:35:52 +0800

    Initial commit

---
+++ src/test/java/com/lzp/javacenter/AppMainTests.java
@@ -0,0 +1,13 @@
+package com.lzp.javacenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMainTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
